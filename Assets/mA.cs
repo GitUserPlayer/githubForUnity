4c794b8452c78660f1ebdbf0b00652b4b355647b
哲 <hkkb8670@gmail.com>
2020-12-24 12:39:22 +0800

    新增程式碼

---
+++ Assets/mA.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mA : MonoBehaviour
+{
+    int a, b;
+    void Start()
+    {
+        a = 5;
+        b = 5;
+        Debug.Log($"startGit{TwoMath(a,b)}");
+    }
+
+    int TwoMath(int num,int num2) 
+    {
+        int answer = num + num2;
+        return answer*num2;
+    }
+}
